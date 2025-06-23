a564349a9535113e35dee518b15355883f51a0bf
muecke36 <michael@muecken.me>
2025-06-23 09:34:20 +0200

    Create install-apps.sh

---
+++ install-apps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mas install 640199958
+mas install 975890633
+mas install 526298438
+mas install 545519333
+mas install 1450874784
+mas install 310633997
+mas install 1295203466
+mas install 497799835
