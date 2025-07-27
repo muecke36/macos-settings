c44f93b2aaf2618fa61671033eac58609105c9fc
muecke36 <michael@muecken.me>
2025-07-27 12:49:54 +0200

    Update install-apps.sh

--- install-apps.sh
+++ install-apps.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-mas install 640199958
-mas install 975890633
-mas install 526298438
-mas install 545519333
-mas install 1450874784
-mas install 310633997
-mas install 1295203466
-mas install 497799835
+mas install 640199958  # Developer
+mas install 975890633  # HotKey
+mas install 526298438  # Lightshot
+mas install 545519333  # Prime Video
+mas install 1450874784 # Transporter
+mas install 310633997  # WhatsApp
+mas install 497799835  # Xcode
+mas install 6469021132 # PDFgear
+mas install 1473079126 # Cleaner One
+mas install 1035236694 # Commander One
